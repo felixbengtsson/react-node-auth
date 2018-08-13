f1a84ccaebb3228f16a062e63f0fdd92617965b3
Felix Bengtsson <felix.bengtsson@me.com>
2018-08-13 20:37:13 +0200

    added feature route

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,18 @@
 import axios from 'axios';
-import { AUTH_USER } from './types';
+import { AUTH_USER, AUTH_ERROR } from './types';
 
-export const signup = (formProps) => dispatch => {
+export const signup = (formProps, callback) => async dispatch => {
   console.log('action called')
-  axios.post('http://localhost:3090/signup', formProps)
+  try {
+
+    const response = await axios.post('http://localhost:3090/signup', formProps)
+    dispatch({ type: AUTH_USER, payload: response.data.token })
+
+    callback()
+  } catch (e) {
+
+    dispatch({ type: AUTH_ERROR, payload: 'Email in use'})
+  }
+  
+
 };
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,9 +1,20 @@
+import {AUTH_USER} from '../actions/types'
+import {AUTH_ERROR} from '../actions/types'
+
 const INITIAL_STATE = {
     authenticated: '',
     errorMessage: ''
   }
   
+
   export default function(state=INITIAL_STATE, action) {
-    return state;
-  
+    switch(action.type) {
+      case AUTH_USER:
+        console.log(state)
+        return {...state, errorMessage: 'Success', authenticated: action.payload }
+      case AUTH_ERROR:
+        return {...state, errorMessage: action.payload }
+      default:
+        return state
+    }
   }
